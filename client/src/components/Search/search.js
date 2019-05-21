9a22185c0803f5b4da605165718f8dc9440309d0
kwonatic <kwonatic@gmail.com>
2019-05-21 13:53:50 -0700

    update

--- client/src/components/Search/search.js
+++ client/src/components/Search/search.js
@@ -1,30 +1,44 @@
-import React, { Fragment } from 'react';
-import PropTypes from 'prop-types';
-import App from '../../../../client/src/App.js'
+import React from 'react';
+import Iframe from 'react-iframe'
+
+// import PropTypes from 'prop-types';
+
+var youTubeKey = 'AIzaSyCLoFEbhSlB0abkW_Ipic1I18qD7-mtHa0'
+
+
+
+
 
 class GoalSearch extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-    
+
         }
 
     }
+
     handleSearch(props) {
         console.log("search")
-        
+
     }
-    
+
+
+
     render() {
-        return(
+        return (
 
             <div>
-                <h1>`Hello, this is {this.props.passData}`</h1>
-                
+                <h1>Here are some search results to help achieving your goal, {this.props.passData}.</h1>
+
             </div>
+            
+
         )
     }
 }
+// -------Below-----youtube------------
 
+//-------Above------Youtube-------------------
 
 export default GoalSearch;
